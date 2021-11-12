fcce669c178ac7b8ee0b2d562ddc645bdb18399c
AntonioAraujo138 <antonioaraujo138@gmail.com>
2021-11-11 22:34:23 -0300

    Update ExercicioFunções.py

---
+++ ExercicioFunções.py
@@ -0,0 +1,10 @@
+def medida(x , y):
+    s = x * y
+    print(f'Comprimento total do Terreno é de {s} ')
+
+
+#Programa Principal
+x = float(input('Informe o a Largura do terreno: '))
+
+y = float(input('Informe o comprimento do terreno: '))
+medida(x , y)
