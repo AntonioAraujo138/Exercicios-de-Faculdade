3de07425673345964e6429e0c57a4fbee1783cc7
AntonioAraujo138 <antonioaraujo138@gmail.com>
2021-11-17 18:46:04 -0300

    criando Menu
    Começo do Projeto

---
+++ ProjetoMenu.py
@@ -0,0 +1,19 @@
+def menu():
+    print(5*'*', 'MENU', 5*'*' )
+    print(5*'-', '[1] Cadastrar produto', 5*'-')
+    print(5*'-', '[2] Cadastrar cliente', 5*'-')
+    print(5*'-', '[3] Realizar venda', 5*'-')
+    print(5*'-', '[4] Imprimir fluxo de caixa', 5*'-')
+    print(5*'-', '[5] Sair', 5*'-' )
+
+def produto():
+    produto = input('Nome do Pruduto:')
+    
+
+menu()
+#executo oque ele quer fazer
+op = input('Digite a opção: ')
+
+
+menu()
+op2 = input('Digite a opção: ')
