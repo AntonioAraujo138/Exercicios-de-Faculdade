0936afe6d87b992e7e0b4709452cc6207e065aff
AntonioAraujo138 <antonioaraujo138@gmail.com>
2021-11-23 21:24:58 -0300

    Formatando Menu

--- ProjetoMenu.py
+++ ProjetoMenu.py
@@ -1,16 +1,8 @@
-def menu():
-    print(5*'*', 'MENU', 5*'*' )
-    print(5*'-', '[1] Cadastrar Produto', 5*'-')
-    print(5*'-', '[2] Cadastrar Cliente', 5*'-')
-    print(5*'-', '[3] Realizar Venda', 5*'-')
-    print(5*'-', '[4] Estoque de Produto', 5*'-')
-    print(5*'-', '[5] Sair', 5*'-' )
-   
+
 
 list_produto = []
 list_cliente = []
-
-
+list_venda = []
 
 def produto():
     while True:
@@ -28,16 +20,56 @@ def cliente():
         if volta in 'Ss':
             break
 
-   
-    
+def venda():
+    while True:
+        list_venda.append(float(input('Valor de Venda: ')))
+        list_venda.append(int(input('Data venda: ')))
+        volta = str(input('Voltar ao Menu [S/N]: '))
+        if volta in 'Ss':
+            break
+
+def menu():
+    print(5*'*', 'MENU', 5*'*' )
+    print(5*'-', '[1] Cadastrar Produto', 5*'-')
+    print(5*'-', '[2] Cadastrar Cliente', 5*'-')
+    print(5*'-', '[3] Vendas do Dia', 5*'-')
+    print(5*'-', '[4] Estoque de Produto', 5*'-')
+    print(5*'-', '[5] Sair', 5*'-' )
+
 
 menu()
+
+op = 0
+while op != 5: 
+
 #executo oque ele quer fazer
-op1 = input('Digite a opção: ')
-produto()
+    op = int(input('Digite a opção: '))
+
+    if op == 1:
+        produto()
+        menu()
+
+    elif op == 2:    
+        cliente()
+        menu()
+
+    elif op ==3:
+        venda()
+        menu()
+
+    elif op == 4:
+        print(list_produto)
+        while True:
+            volta = str(input('Voltar ao Menu [S/N]: '))
+            if volta in 'Ss':
+                break
+        menu()
+
+    elif op == 5:
+        print(5*'*','Finalizando..... ',5*'*')
+    else:
+        print('Opção inválida. Tente Novamente. ')
+
+print('\n Fim do Programa.')
 
 
-
-menu()
-op2 = input('Digite a opção: ')
-print(list_produto)
